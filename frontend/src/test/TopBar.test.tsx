3d8ff653a3502cb9a71e4c9231a3863ed24aec85
Kyle Pacheco <kpacheco@nnu.edu>
2021-11-11 19:35:45 -0700

    Starting testing

--- frontend/src/test/TopBar.test.tsx
+++ frontend/src/test/TopBar.test.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import TestHook from "./TestHook";
 import { render, fireEvent, cleanup } from "@testing-library/react";
+import { TopBar } from "../components/TopBar";
 
 afterEach(cleanup);
 
 it("Text in state is changed when button clicked", () => {
-  const { getByText } = render(<TestHook />);
+  const { getByText } = render(<TopBar />);
 
   expect(getByText(/Initial/i).textContent).toBe("Initial State");
 
