1cbe71eb586dbc9cfe0840e4b113165c33b9968f
Kyle Pacheco <kpacheco@nnu.edu>
2021-11-12 17:28:04 -0700

    TopBar test

--- frontend/src/test/TopBar.test.tsx
+++ frontend/src/test/TopBar.test.tsx
@@ -1,16 +1,17 @@
+import { cleanup, render } from "@testing-library/react";
 import React from "react";
-import ReactDOM from "react-dom";
-import { render, fireEvent, cleanup } from "@testing-library/react";
 import { TopBar } from "../components/TopBar";
+
+const mockedUsedNavigate = jest.fn();
+
+jest.mock("react-router-dom", () => ({
+  ...(jest.requireActual("react-router-dom") as any),
+  useNavigate: () => mockedUsedNavigate,
+}));
 
 afterEach(cleanup);
 
-it("Text in state is changed when button clicked", () => {
-  const { getByText } = render(<TopBar />);
-
-  expect(getByText(/Initial/i).textContent).toBe("Initial State");
-
-  fireEvent.click(getByText("State Change Button"));
-
-  expect(getByText(/Initial/i).textContent).toBe("Initial State Changed");
+it("renders", async () => {
+  const { asFragment } = render(<TopBar />);
+  expect(asFragment()).toMatchSnapshot();
 });
